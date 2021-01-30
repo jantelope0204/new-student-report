874a46cb61b4c6eea65cfd83a82ddb4a42509709
lingyang <jantelope@163.com>
2021-01-30 15:05:57 +0800

    lingyang

--- new-student/src/main/java/com/ruoyi/utils/CreateStudent.java
+++ new-student/src/main/java/com/ruoyi/utils/CreateStudent.java
@@ -17,7 +17,7 @@ public class CreateStudent {
     public static final String NATIVE = "中国";
     public static final Integer number = 0;
     public static final String [] POLITICAL = {"群众","团员","党员"};
-    public static final String [] NATION = {"包括蒙古族", "回族", "藏族", "维吾尔族", "苗族", "彝族", "壮族",
+    public static final String [] NATION = {"蒙古族", "回族", "藏族", "维吾尔族", "苗族", "彝族", "壮族",
             "布依族", "朝鲜族", "满族", "侗族", "瑶族", "白族", "土家族", "哈尼族", "哈萨克族",
             "傣族", "黎族", "傈僳族", "佤族", "畲族", "高山族", "拉祜族", "水族", "东乡族", "纳西族",
             "景颇族", "柯尔克孜族", "土族", "达斡尔族", "仫佬族", "羌族", "布朗族", "撒拉族", "毛南族", "仡佬族", "锡伯族", "阿昌族", "普米族",
